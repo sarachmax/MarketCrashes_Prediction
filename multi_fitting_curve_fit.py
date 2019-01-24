1af4a26f2147793bbad6efa37877907094ebaad7
sarach rujiranurak <sarach.r@ku.th>
2019-01-24 20:40:49 +0700

    add files

---
+++ multi_fitting_curve_fit.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 20:39:30 2019
+
+@author: sarac
+"""
+
+import numpy as np
+import scipy.stats as scs
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+
+
+# Log periodic function
+def y(x, o, m, A, B, C, tau):
+    return A - B * tau**m * x**m + C * tau**m * x**m * np.cos(o * np.log(x))
+
+
+def res_fun(params, x, y_fit):
+    return y(x, params[0], params[1], params[2], params[3], params[4],
+             params[5]) - y_fit
+
+
+# Log periodic function #2
+def y_2(x, o, m, tau):
+    ''' Function of the nonlinear parameters o, m, tau only.  Solves
+        for the optimal 'slave' parameters X_hat(o, m, tau)
+        OUTPUT: Function value
+    '''
+    A_hat, B_hat, C_1_hat, C_2_hat = minABC_y(xd, o, m, tau)
+
+    return A_hat \
+           + B_hat * (tau - x)**m \
+           + C_1_hat * (tau - x)**m * np.cos(o * np.log(tau - x)) \
+           + C_2_hat * (tau - x)**m * np.sin(o * np.log(tau - x))
+
+def res_fun_2(params, x, y_fit):
+    ''' Residual function -- required for the minimization problem
+        OUTPUT: Residual
+    '''
+    return y_2(x, params[0], params[1], params[2]) - y_fit
+
+
+def minABC_y(x, o, m, tau):
+    ''' Solves for the 'slave' variables A, B, C_1, C_2
+        INPUT: Nonlinear parameters o, m, tau
+        OUTPUT: Returns linear parameter solutions (A, B, C_1, C_2)
+    '''
+    yi = yd
+    fi = (tau - x)**m
+    gi = (tau - x)**m * np.cos(o * np.log(tau - x))
+    hi = (tau - x)**m * np.sin(o * np.log(tau - x))
+    N = float(len(x))
+
+    mat = np.array([[N, np.sum(fi), np.sum(gi), np.sum(hi)],
+                    [np.sum(fi), fi.dot(fi), fi.dot(gi), fi.dot(hi)],
+                    [np.sum(gi), fi.dot(gi), gi.dot(gi), gi.dot(hi)],
+                    [np.sum(hi), fi.dot(hi), gi.dot(hi), hi.dot(hi)]])
+
+    s_vec = np.array([np.sum(yi),
+                      yi.dot(fi),
+                      yi.dot(gi),
+                      yi.dot(hi)]).reshape(-1, 1)
+    vec_hat = np.linalg.inv(mat).dot(s_vec)
+
+    return tuple(vec_hat.flatten())
+
+def init_fit_params(data): 
+    # Fit parameters 
+    o = 20.0    #omega
+    m = 1.0
+    A = 1.0
+    B = -1.0
+    C = 0.5
+    t = 3.0     #tc 
+    sp = data[-1]
+    p0 = (sp, m, A, B, C, t)
+    return p0 
+
+if __name__ == '__main__':
+    # dataset 
+#    dataset = pd.read_csv("Dataset/SET_1D.csv")
+    dataset = pd.read_csv("Dataset/SEHK_30Min_2800.csv")
+    
+    raw_data = dataset.Close
+    data = np.array(dataset.Close)
+    data = np.log(data)
+    
+    
+    window_sizes = [120, 240, 480, 640, 960] 
+    mean_crash_point = [] 
+    all_crash_point = [] 
+    crash_point = {} 
+    for size in window_sizes :
+        crash_point[size] = []
+    start = np.max(window_sizes)
+    stop = len(data) 
+    jump_size = np.min(window_sizes)
+    max_windows = np.max(window_sizes)
+    num_windows = len(window_sizes)
+    for i in range(start, stop, jump_size):
+        c_point = [] 
+        print("Fitting on data point on " + str(i) + " to " + str(i+max_windows))
+        for size in window_sizes : 
+            xd = np.linspace(0.1, size, size)
+            yd = data[i-size:i]
+            
+            p0 = init_fit_params(yd)
+            maxfev = 5000
+            while True : 
+                try :
+                    popt, pcov = curve_fit(y, xd, yd, p0=p0, maxfev=maxfev)
+                    break 
+                except KeyboardInterrupt : 
+                    break
+                except : 
+                    if maxfev >= 20000 :
+                        print("No fitting point on " + str(i) + " to " + str(i+max_windows))
+                        break 
+                    else : 
+                        maxfev = 20000
+            c_time = int(popt[5]+i+size)
+            if c_time > i + size  and c_time < stop: 
+                crash_point[size].append(c_time)
+                c_point.append(c_time)
+                all_crash_point.append(c_time)
+            median_point = np.median(c_point) 
+            if not np.isnan(median_point) : 
+                mean_crash_point.append(median_point)
+    
+    plot_all = False 
+    plot_mean = True
+    plot_each_window= True 
+    
+    if plot_all : 
+        survive_time = [] 
+        mem = [] 
+        for x in all_crash_point : 
+            if not (x in mem):
+                mem.append(x)
+            else : 
+                if not (x in survive_time):
+                    survive_time.append(x) 
+        plt.figure()         
+        plt.title("Crash point all")
+        plt.plot(raw_data, label='prices')
+        for x in all_crash_point :
+            plt.axvline(x, color='red')
+        plt.legend()
+        plt.show();
+    
+    if plot_mean :     
+        survive_time = [] 
+        mem = [] 
+        for x in mean_crash_point : 
+            if not (x in mem):
+                mem.append(x)
+            else : 
+                if not (x in survive_time):
+                    survive_time.append(x) 
+        plt.figure(figsize=(1920,1080), dpi=80) 
+        plt.title("Crash point mean")
+        plt.plot(raw_data, label='prices')
+        for x in survive_time :
+            plt.axvline(x, color='green')
+        plt.legend()
+        plt.show();
+    
+    if plot_each_window :     
+        for size in window_sizes : 
+            plt.figure()
+            title = 'Fit on '+ str(size) + ' windows size'
+            plt.title(title)
+            plt.plot(raw_data)
+            for x in crash_point[size] :
+                plt.axvline(x, color='red')
+            plt.show()
+                
+        
+            
+        
+            
+
+    
